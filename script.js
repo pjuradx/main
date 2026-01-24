ec74835b7fa077da2a011873e16a4c9482be4739
pablo. <35315891+pjuradx@users.noreply.github.com>
2026-01-24 16:08:46 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var randomNumber = Math.floor((Math.random() * 221) + 1);
-  var randomBackground = randomNumber + ".jpg";
-  console.log(randomNumber);
-
-  document.querySelector("div#picture").style.backgroundImage = "url(src/a/" + randomBackground + ")";
-  document.querySelector("div.background").style.backgroundImage = "url(src/a/" + randomBackground + ")";
-});
